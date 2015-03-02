13503158f6bacef7bb869b70c490798d6a5f841e
Jesseract <jlpearce@gmail.com>
2015-03-02 16:25:24 -0500

    Initial commit

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,22 @@
+class ProfileController < ApplicationController
+  # def avatar_url
+  #   @avatar "https://avatars.githubusercontent.com/u/10173258?v=3"
+  #   @avatar
+  # end
+
+  def index
+    profile_url = "https://api.github.com/users/jesseract"
+    @profile = HTTParty.get(profile_url, login_info)
+
+    avatar_url = "https://api.github.com/users/jesseract"
+    @avatar = HTTParty.get(avatar_url, login_info)["avatar_url"]
+
+  end
+
+  private def login_info
+    {headers: {"Authorization" => "token #{ENV['GITHUB_TOKEN']}",
+               "User-Agent" => "jesseract"}
+    }
+  end
+
+end
